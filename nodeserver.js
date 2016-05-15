0646445bef42950bbabfee3d9142fc1b1d1f96e3
Colby Butler <colby@owljet.com>
2016-05-15 12:53:08 -0400

    Updated path in nodeserver.js

--- nodeserver.js
+++ nodeserver.js
@@ -6,10 +6,10 @@ var fs = require("fs");
 
 http.createServer(function(request,response){
 	var my_path = url.parse(request.url).pathname;
-	var full_path = path.join(process.cwd(), '/', my_path);
+	var full_path = path.join(process.cwd(), '/www/', my_path);
 
   var closurePath = path.join(process.cwd(), '/../../../third_party/src/closure/closure/');
-  
+
 
   if (my_path.indexOf('/closure') == 0) {
     full_path = path.join(process.cwd(), '/../../../third_party/src/closure/', my_path);
@@ -20,26 +20,26 @@ http.createServer(function(request,response){
 
 	fs.exists(full_path,function(exists){
 		if(!exists){
-			response.writeHeader(404, {"Content-Type": "text/plain"});  
-			response.write("404 Not Found\n");  
+			response.writeHeader(404, {"Content-Type": "text/plain"});
+			response.write("404 Not Found\n");
 			response.end();
 		}
 		else{
-			fs.readFile(full_path, "binary", function(err, file) {  
-			     if(err) {  
-			         response.writeHeader(500, {"Content-Type": "text/plain"});  
-			         response.write(err + "\n");  
-			         response.end();  
-			   
-			     }  
+			fs.readFile(full_path, "binary", function(err, file) {
+			     if(err) {
+			         response.writeHeader(500, {"Content-Type": "text/plain"});
+			         response.write(err + "\n");
+			         response.end();
+
+			     }
 				 else{
-					response.writeHeader(200);  
-			        response.write(file, "binary");  
+					response.writeHeader(200);
+			        response.write(file, "binary");
 			        response.end();
 				}
-					 
+
 			});
 		}
 	});
 }).listen(9378);
-sys.puts("Server Running on 9378");			
+sys.puts("Server Running on 9378");
